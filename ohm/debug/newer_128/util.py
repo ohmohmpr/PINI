5e400db0e2929ca8d3f0af9a0f2661f48f038185
Panyawat Rattana <nanoic_ohm@hotmail.com>
2025-09-10 14:26:32 +0200

    [ADD]: typo

--- ohm/debug/newer_128/util.py
+++ ohm/debug/newer_128/util.py
@@ -25,14 +25,14 @@ def plot_state(pred_data_LIO_EKF, update_data_LIO_EKF, pred_data_PINI, update_da
     label_idx = 15
 
   figure, axis = plt.subplots(3, 1, sharex=True, sharey=True, figsize=(12, 9))
-  
-  axis[0].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx  , start_time_idx:end_time_idx], 'r--', label='x_LIO_EKF') # x
-  axis[1].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx+1, start_time_idx:end_time_idx], 'g--', label='y_LIO_EKF') # y
-  axis[2].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx+2, start_time_idx:end_time_idx], 'b--', label='z_LIO_EKF') # z
 
   axis[0].plot(pred_data_PINI[0, start_time_idx:end_time_idx] - pred_data_PINI[0,0], pred_data_PINI[label_idx  , start_time_idx:end_time_idx], 'r-', label='x_PINI') # x
   axis[1].plot(pred_data_PINI[0, start_time_idx:end_time_idx] - pred_data_PINI[0,0], pred_data_PINI[label_idx+1, start_time_idx:end_time_idx], 'g-', label='y_PINI') # y
   axis[2].plot(pred_data_PINI[0, start_time_idx:end_time_idx] - pred_data_PINI[0,0], pred_data_PINI[label_idx+2, start_time_idx:end_time_idx], 'b-', label='z_PINI') # z
+
+  axis[0].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx  , start_time_idx:end_time_idx], 'k--', label='x_LIO_EKF') # x
+  axis[1].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx+1, start_time_idx:end_time_idx], 'k--', label='y_LIO_EKF') # y
+  axis[2].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx+2, start_time_idx:end_time_idx], 'k--', label='z_LIO_EKF') # z
 
   axis[0].set_title("X")
   axis[1].set_title("Y")
@@ -61,13 +61,13 @@ def plot_delta_x(pred_data_LIO_EKF, update_data_LIO_EKF, pred_data_PINI, update_
 
   figure, axis = plt.subplots(3, 1, sharex=True, sharey=True, figsize=(12, 9))
   
-  axis[0].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx  , start_time_idx:end_time_idx], 'r--', label='x_LIO_EKF') # x
-  axis[1].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx+1, start_time_idx:end_time_idx], 'g--', label='y_LIO_EKF') # y
-  axis[2].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx+2, start_time_idx:end_time_idx], 'b--', label='z_LIO_EKF') # z
-
   axis[0].plot(pred_data_PINI[0, start_time_idx:end_time_idx] - pred_data_PINI[0,0], pred_data_PINI[label_idx  , start_time_idx:end_time_idx], 'r-', label='x_PINI') # x
   axis[1].plot(pred_data_PINI[0, start_time_idx:end_time_idx] - pred_data_PINI[0,0], pred_data_PINI[label_idx+1, start_time_idx:end_time_idx], 'g-', label='y_PINI') # y
   axis[2].plot(pred_data_PINI[0, start_time_idx:end_time_idx] - pred_data_PINI[0,0], pred_data_PINI[label_idx+2, start_time_idx:end_time_idx], 'b-', label='z_PINI') # z
+
+  axis[0].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx  , start_time_idx:end_time_idx], 'k--', label='x_LIO_EKF') # x
+  axis[1].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx+1, start_time_idx:end_time_idx], 'k--', label='y_LIO_EKF') # y
+  axis[2].plot(pred_data_LIO_EKF[0, start_time_idx:end_time_idx] - pred_data_LIO_EKF[0,0], pred_data_LIO_EKF[label_idx+2, start_time_idx:end_time_idx], 'k--', label='z_LIO_EKF') # z
 
   axis[0].set_title("X")
   axis[1].set_title("Y")
